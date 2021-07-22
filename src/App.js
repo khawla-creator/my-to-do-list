2138bf6d642913229e6c9d922221335402dfcedd
khawla guesmi <85040684+khawla-creator@users.noreply.github.com>
2021-07-22 19:36:16 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./css/main.css";
+import Todos from './componenets/Todos'
+import DisplayTodos from "./componenets/DisplayTodos";
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Todo App</h1>
+<Todos/>
+<DisplayTodos/>
+    </div>
+  );
+}
+ export default App;
